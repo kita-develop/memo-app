6439895c657c0ec2f8847f389dab59880ac8b843
kita-dev <78951631+kita-develop@users.noreply.github.com>
2022-01-25 18:36:09 +0900

    Create DetailScreen

--- App.jsx
+++ App.jsx
@@ -1,24 +1,11 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import MemoDetailScreen from './src/screens/MemoDetailScreen';
 
-import AppBar from './src/components/AppBar';
-import CircleButton from './src/components/CircleButton';
-import MemoList from './src/components/MemoList';
+import MemoListScreen from './src/screens/MemoListScreen';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <AppBar />
-      <MemoList />
-      <CircleButton>+</CircleButton>
-    </View>
+    // <MemoListScreen />
+    <MemoDetailScreen />
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#F0F4F8',
-  },
-
-});
